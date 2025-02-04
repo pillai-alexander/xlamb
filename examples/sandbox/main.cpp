fe2a8d57b4f24ab6ca9dbfef077288d42270cb53
Alexander N. Pillai <pillai.alexander.n@gmail.com>
2025-02-04 15:25:47 -0600

    more sandbox testing

--- examples/sandbox/main.cpp
+++ examples/sandbox/main.cpp
@@ -1,52 +1,94 @@
 #include <xlamb/xlamb.hpp>
 
-struct SusceptibilityComponent {
-    float susceptibility;
+#include <unordered_map>
+#include <random>
+
+enum class Strain {
+    FLU,
+    NON_FLU,
+    NUM_STRAINS
+};
+
+struct Pathogen {
+    Strain strain;
+    float pr_exposure;
+};
+
+struct Susceptibility {
+    Susceptibility() {
+        baseline_susceptibility[Strain::FLU]     = 1.0;
+        baseline_susceptibility[Strain::NON_FLU] = 1.0;
+        current_susceptibility[Strain::FLU]      = 1.0;
+        current_susceptibility[Strain::NON_FLU]  = 1.0;
+    };
+    std::unordered_map<Strain, float> baseline_susceptibility;
+    std::unordered_map<Strain, float> current_susceptibility;
+};
+
+struct Infection {
+    size_t time;
+    Strain strain;
+};
+
+struct InfectionHistory {
+    std::vector<Infection> inf_hist;
+};
+
+struct Vaccination {
+    size_t time;
+    std::unordered_map<Strain, float> efficacy;
+};
+
+struct VaccinationHistory {
+    std::vector<Vaccination> vax_hist;
 };
 
 int main() {
-    // testing logging
-    XLAMB_TRACE("trace");
-    XLAMB_DEBUG("debug");
-    XLAMB_INFO("info");
-    XLAMB_WARN("warn");
-    XLAMB_ERROR("error");
-    XLAMB_CRITICAL("critical");
+    std::default_random_engine rng(0);
+    std::uniform_real_distribution<> unif(0.0, 1.0);
 
     xlamb::Architect architect;
     auto& context = architect.context;
 
-    for(auto i = 0u; i < 10u; ++i) {
-        const auto entity = context.create_entity();
-        context.destroy_entity(entity);
-    }
+    auto flu = context.create_entity("flu_pathogen");
+    flu.add_component<Pathogen>(Strain::FLU, 0.01);
 
-    auto default_entity = context.create_entity();
-    XLAMB_INFO("default name = {}", default_entity.name());
-    context.destroy_entity(default_entity);
-
-    auto named_entity = context.create_entity("named_entity");
-    named_entity.add_component<SusceptibilityComponent>(1.0);
-    XLAMB_INFO("entity ({}) w/ suscep = {}",
-      named_entity.name(),
-      named_entity.get_component<SusceptibilityComponent>().susceptibility
-    );
-
-    context.clear_registry();
+    auto nflu = context.create_entity("nonflu_pathogen");
+    nflu.add_component<Pathogen>(Strain::NON_FLU, 0.001);
 
     for (int i = 0; i < 10; ++i) {
-        auto ent = context.create_entity("entity_" + std::to_string(i));
-        const auto baseline_suscep = (i < 5) ? 0.5 : 1.0;
-        ent.add_component<SusceptibilityComponent>(baseline_suscep);
+        auto p = context.create_entity("person_" + std::to_string(i));
+        p.add_component<Susceptibility>();
+        p.add_component<InfectionHistory>();
+        p.add_component<VaccinationHistory>();
     }
 
-    auto view = context.view_entities_with<xlamb::TagComponent, SusceptibilityComponent>();
-    for (auto ent : view) {
-        const auto [tag, suscep] = view.get(ent);
+    auto vax = Vaccination();
+    vax.efficacy[Strain::FLU]     = 0.5;
+    vax.efficacy[Strain::NON_FLU] = 0.0;
 
-        const auto n = tag.name;
-        const auto s = suscep.susceptibility;
-        XLAMB_INFO("{} suscep = {}", n, s);
+    auto people_to_be_vaxd = context.view_entities_with<VaccinationHistory, Susceptibility>();
+    for (auto ent : people_to_be_vaxd) {
+        if (unif(rng) < 0.5) {
+            auto [vh, s] = people_to_be_vaxd.get(ent);
+            vh.vax_hist.push_back(vax);
+            s.current_susceptibility.at(Strain::FLU) *= vax.efficacy.at(Strain::FLU);
+            s.current_susceptibility.at(Strain::NON_FLU) *= vax.efficacy.at(Strain::NON_FLU);
+        }
+    }
+
+    auto synth_pop = context.view_entities_with<Susceptibility>();
+    for (auto ent : synth_pop) {
+        auto [s] = synth_pop.get(ent);
+        const auto curr_suscep = s.current_susceptibility.at(Strain::FLU);
+        XLAMB_TRACE("entity flu suscep = {}", curr_suscep);
+    }
+
+    auto pathogens = context.view_entities_with<Pathogen>();
+    for (auto ent : pathogens) {
+        auto [path] = pathogens.get(ent);
+        auto strain = (path.strain == Strain::FLU) ? "flu" : "non_flu";
+        XLAMB_TRACE("pathogen {}: Pr(exposure) = {}", strain, path.pr_exposure);
     }
 
 /* API PLANNING
